19c5340419f511c07ff67a1904bc93647d1786f7
Kevin Castillo <kevinacastles@gmail.com>
2024-06-10 00:59:09 -0400

    fix: semantic html

--- src/app/app/[id]/layout.tsx
+++ src/app/app/[id]/layout.tsx
@@ -2,12 +2,12 @@ import Navbar from '../../components/navbar';
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-4">
-      <div className="col-span-4 md:col-span-1">
+    <main className="grid grid-cols-4">
+      <aside className="col-span-4 md:col-span-1">
         <Navbar />
-      </div>
-      <div className="col-span-4 md:col-span-3 bg-zinc-200 p-6 md:overflow-y-auto md:p-12">{children}</div>
-    </div>
+      </aside>
+      <article className="col-span-4 md:col-span-3 bg-zinc-200 p-6 md:overflow-y-auto md:p-12">{children}</article>
+    </main>
   );
 }
 
