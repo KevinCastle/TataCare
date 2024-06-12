c60b6f4422f68f8ff07672fb31394f6ba30a73e4
Kevin Castillo <kevinacastles@gmail.com>
2024-06-12 03:49:53 -0400

    feat: edit and add methods to change databasse

--- src/app/api/data.ts
+++ src/app/api/data.ts
@@ -10,10 +10,7 @@ export async function fetchElders() {
   try {
     const data = await sql<Elder>`
       SELECT
-        id,
-        name,
-        surname,
-        birthdate,
+        *
       FROM elders
       ORDER BY name ASC
     `;
@@ -26,24 +23,70 @@ export async function fetchElders() {
   }
 }
 
-export async function getElderById(id: string | string[]) {
-  let elderId = '';
-  if (Array.isArray(id)) {
-    [elderId] = Array.isArray(id) ? id : [id];
-  }
+export async function getElderById(id: string) {
   noStore();
   try {
     const elder = await sql<Elder>`
       SELECT
         *
       FROM elders
-      WHERE id=${elderId}
+      WHERE id=${id}
     `;
 
     return elder.rows[0] as Elder;
   } catch (err) {
     console.error('Database Error:', err);
     throw new Error('Failed to fetch elder.');
+  }
+}
+
+export async function addElder(elder: Elder) {
+  noStore();
+  const keys = Object.keys(elder).join(', ');
+  const values = Object.values(elder).join(', ');
+  try {
+    const elder = await sql<Elder>`
+      INSERT INTO elders (${keys})
+        VALUES (${values})
+    `;
+
+    return elder.rows[0] as Elder;
+  } catch (err) {
+    console.error('Database Error:', err);
+    throw new Error('Failed to create elder.');
+  }
+}
+
+export async function updateElder(id: string, key: string, data: string | boolean | number) {
+  noStore();
+  try {
+    const elder = await sql<Elder>`
+      UPDATE elders
+      SET ${key}=${data}
+      WHERE id=${id}
+    `;
+
+    return elder.rows[0] as Elder;
+  } catch (err) {
+    console.error('Database Error:', err);
+    throw new Error('Failed to fetch elder.');
+  }
+}
+
+export async function deleteElder(id: string) {
+  try {
+    const result = await sql`
+      DELETE FROM elders
+      WHERE id=${id}
+      RETURNING *;
+    `;
+    if (result.rowCount === 0) {
+      throw new Error('Elder not found.');
+    }
+    return result.rows[0] as Elder; // Returns the deleted elder's data
+  } catch (err) {
+    console.error('Database Error:', err);
+    throw new Error('Failed to delete elder.');
   }
 }
 
