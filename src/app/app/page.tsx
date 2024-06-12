b60215511cdb4da91ca5679e9627ee47495c0ef5
Kevin Castillo <kevinacastles@gmail.com>
2024-06-12 03:52:33 -0400

    fix: create a client side component only for elderCardList

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,16 +1,11 @@
-import {
-  Button, Card, CardBody, CardFooter, Image,
-} from '@nextui-org/react';
-import { CaretRight, SignOut } from '@phosphor-icons/react/dist/ssr';
-import Link from 'next/link';
+import { Button } from '@nextui-org/react';
+import { SignOut } from '@phosphor-icons/react/dist/ssr';
 import { signOut } from '../../auth';
+import ElderCardList from '../components/elderCardList';
 import { fetchElders } from '../api/data';
 
 const page = async () => {
   const elders = await fetchElders();
-
-  const fullName = (name: string, surname: string) => `${name} ${surname}`;
-
   return (
     <main className="bg-zinc-200 min-h-svh relative">
       <form
@@ -35,32 +30,7 @@ const page = async () => {
         <section className="row-span-1 flex items-center justify-center h-[25vh]">
           <h1 className="text-3xl text-center">Lista de abuelitos</h1>
         </section>
-        <section className="row-span-2 container flex flex-col h-full px-6 xs:px-0">
-          <div className="flex flex-wrap justify-center items-center gap-6">
-            {elders.map((elder) => (
-              <Link href={`/app/${elder.id}`} scroll={false} key={elder.id}>
-                <Card className="xs:flex-[0_0_45%] sm:flex-[0_0_33.3333%] lg:flex-[0_0_23%] w-full sm:w-[200px]" shadow="sm" key={elder.id} isPressable>
-                  <CardBody className="overflow-visible p-0">
-                    <Image
-                      shadow="sm"
-                      radius="lg"
-                      width="100%"
-                      alt={fullName(elder.name, elder.surname)}
-                      className="w-full object-cover object-top h-[200px]"
-                      src="https://images.unsplash.com/photo-1612838320302-4b3b3b3b3b3b"
-                    />
-                  </CardBody>
-                  <CardFooter className="text-small justify-between">
-                    <p className="text-xl font-bold truncate">{fullName(elder.name, elder.surname)}</p>
-                    <span className="text-tiny h-8 w-0 flex justify-center items-center text-white bg-cyan-700/60 min-w-8 p-0 rounded-full">
-                      <CaretRight size={16} />
-                    </span>
-                  </CardFooter>
-                </Card>
-              </Link>
-            ))}
-          </div>
-        </section>
+        <ElderCardList elders={elders} />
       </article>
     </main>
   );
