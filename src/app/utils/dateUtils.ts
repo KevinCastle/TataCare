9750afb2501aa9fe47aab6c145076b6c7830836a
Kevin Castillo <kevinacastles@gmail.com>
2024-06-16 22:21:35 -0400

    feat: add util function for dateTime

--- src/app/utils/dateUtils.ts
+++ src/app/utils/dateUtils.ts
@@ -9,3 +9,23 @@ export const getAge = (birthdate: string): string => {
   const age = new Date(Date.now() - date.getTime());
   return Math.abs(age.getUTCFullYear() - 1970).toString();
 };
+
+export const formatDateTime = (dateString: string): string => {
+  const date = new Date(dateString);
+
+  date.setHours(18, 17);
+
+  const formattedDate = date.toLocaleDateString('es-CL', {
+    month: 'short',
+    day: '2-digit',
+    year: '2-digit',
+  });
+
+  const formattedTime = date.toLocaleTimeString('en-US', {
+    hour: 'numeric',
+    minute: '2-digit',
+    hour12: true,
+  }).toLowerCase();
+
+  return `${formattedTime} ${formattedDate}`;
+};
