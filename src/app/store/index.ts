416f0468ffb39a505ccd05617fb4f2d768a67f04
Kevin Castillo <kevinacastles@gmail.com>
2024-06-16 22:21:12 -0400

    feat: create an index for stores

---
+++ src/app/store/index.ts
@@ -0,0 +1,7 @@
+export { useAllergyStore } from './allergyStore';
+export { useDiseaseStore } from './diseaseStore';
+export { useTasteStore } from './tasteStore';
+export { useCommentStore } from './commentStore';
+export { useContactStore } from './contactStore';
+export { useElderStore } from './elderStore';
+export { useMedicationStore } from './medicationStore';
